16681db238366b7922becf6c183f2e9b2ed1234d
MGRINZ <24416088+MGRINZ@users.noreply.github.com>
2022-05-13 19:42:29 +0200

    Threshold calculations results

--- _utils/_calc_tresh.py
+++ _utils/_calc_tresh.py
@@ -8,15 +8,16 @@ import cv2 as cv
 from alg import ReferenceAlgorithm
 import os
 
-img_file = "img0.jpg"
+for img_file in ["img0.jpg", "img1.jpg", "img2.jpg"]:
+    print(img_file)
+    with open(f"_calc_tresh_results_moderate_samples_{img_file}.csv", "w") as f:
+        f.write("s_thresh;v_thresh;count\n")
+        
+        for s_thresh in range(0, 256):
+            for v_thresh in range(0, 256):
+                a = ReferenceAlgorithm(f"../imgs/moderate_samples/{img_file}", s_thresh,v_thresh)
+                a.count()
+                f.write(f"{s_thresh};{v_thresh};{len(a.blobs)}\n")
 
-with open(f"_calc_tresh_results_{img_file}.csv", "w") as f:
-    f.write("s_thresh;v_thresh;count\n")
-    
-    for s_thresh in range(0, 256):
-        for v_thresh in range(0, 256):
-            a = ReferenceAlgorithm(f"../imgs/easy_samples/{img_file}", s_thresh,v_thresh)
-            a.count()
-            f.write(f"{s_thresh};{v_thresh};{len(a.blobs)}\n")
-
-            print(f"\rProgress: {(s_thresh * 256 + v_thresh)/256/256 * 100:.2f}%", end="")
+                print(f"\rProgress: {(s_thresh * 256 + v_thresh)/256/256 * 100:.2f}%", end="")
+        print()
