bac17125425d127b973cf9f235404717589a674b
MGRINZ <24416088+MGRINZ@users.noreply.github.com>
2022-06-28 17:42:54 +0200

    Change V thresholding

--- alg/custom.py
+++ alg/custom.py
@@ -1,7 +1,7 @@
 from . import *
 from alg.ref import ReferenceAlgorithm
 import hist_filter as hf
-from scipy import optimize
+from scipy import optimize, signal
 
 class CustomHsvBlobAlgorithm(ReferenceAlgorithm):
 
@@ -42,6 +42,9 @@ class CustomHsvBlobAlgorithm(ReferenceAlgorithm):
         self.img_s_h_masked = cv.bitwise_and(self.img_prep_s, self.h_mask)
         self.img_v_h_masked = cv.bitwise_and(cv.bitwise_not(self.img_prep_v), self.h_mask)
 
+        # self.img_s_h_masked = cv.GaussianBlur(self.img_s_h_masked, (3, 3), 0)
+        self.img_v_h_masked = cv.GaussianBlur(self.img_v_h_masked, (3, 3), 0)
+
         s_hist = self._get_single_channel_histogram(self.img_s_h_masked)[1:]
         v_hist = self._get_single_channel_histogram(self.img_v_h_masked)[1:]
 
@@ -49,7 +52,12 @@ class CustomHsvBlobAlgorithm(ReferenceAlgorithm):
         (a, mu, sigma) = self._fit_gauss(x, s_hist)
         s_pix_val = np.ceil(mu + 1.5 * sigma)
 
-        v_pix_val = v_hist.tolist().index(v_hist.max()) + 0
+        # peaks, _ = signal.find_peaks(np.append(v_hist, 0), np.average(v_hist))
+        # v_pix_val = np.max(peaks)
+
+        # v_pix_val = v_hist.tolist().index(v_hist.max()) + 0
+
+        v_pix_val = 254
 
         print(s_pix_val)
         print(v_pix_val)
@@ -125,32 +133,51 @@ class CustomHsvBlobAlgorithm(ReferenceAlgorithm):
         return a / (sigma * np.sqrt(2 * np.pi)) * np.exp(-(x - mu)**2 / (2 * sigma**2))
 
     def _morphology(self):
-        # self.img_v_morphed = cv.morphologyEx(self.img_v_morphed, cv.MORPH_ERODE, (5,5))
-
-        self.img_s_morphed = self._morph_channel(np.copy(self.img_s_thresh))
-        self.img_v_morphed = self._morph_channel(np.copy(self.img_v_thresh))
-
-        # self.img_v_morphed = self._closing_opening(self.img_v_morphed)
-        # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
-        # self.img_v_morphed = cv.morphologyEx(self.img_v_morphed, cv.MORPH_ERODE, kernel)
-        # self.img_v_morphed = self._opening_closing(self.img_v_morphed, open_ksize=(11, 11))
+        self.img_s_morphed = self._morph_s_channel(np.copy(self.img_s_thresh))
+        self.img_v_morphed = self._morph_v_channel(np.copy(self.img_v_thresh))
 
         # BEGIN for now
         self.img_bitwise = cv.bitwise_and(self.img_s_thresh, self.img_v_thresh)
         self.img_morphed = self.img_bitwise
         # END for now
 
-    def _morph_channel(self, channel):
+    def _morph_s_channel(self, channel):
         # _, _, stats, _ = cv.connectedComponentsWithStats(channel, connectivity=8)
         # for s in stats:
         #     if s[cv.CC_STAT_AREA] <= 1:
         #         channel[s[cv.CC_STAT_TOP], s[cv.CC_STAT_LEFT]] = 0
 
-        contours, hierarchy = cv.findContours(channel, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-        for c in contours:
-            if cv.contourArea(c) <= 1: # TODO: 4 px != 1 px
-                cv.drawContours(channel, [c], 0, 0, -1)
-        # return channel
+        # contours, hierarchy = cv.findContours(channel, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        # for c in contours:
+        #     if cv.contourArea(c) <= 1: # TODO: 4 px != 1 px
+        #         cv.drawContours(channel, [c], 0, 0, -1)
+
+        return channel
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
+        channel = cv.morphologyEx(channel, cv.MORPH_DILATE, kernel)
+        channel = self._fill_holes(channel)
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
+        channel = cv.morphologyEx(channel, cv.MORPH_ERODE, kernel)
+
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+        channel = cv.morphologyEx(channel, cv.MORPH_OPEN, kernel, iterations=3)
+        return channel
+
+    def _morph_v_channel(self, channel):
+        # _, _, stats, _ = cv.connectedComponentsWithStats(channel, connectivity=8)
+        # for s in stats:
+        #     if s[cv.CC_STAT_AREA] <= 1:
+        #         channel[s[cv.CC_STAT_TOP], s[cv.CC_STAT_LEFT]] = 0
+
+        # contours, hierarchy = cv.findContours(channel, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        # for c in contours:
+        #     if cv.contourArea(c) <= 1: # TODO: 4 px != 1 px
+        #         cv.drawContours(channel, [c], 0, 0, -1)
+
+        # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
+        # channel = cv.morphologyEx(channel, cv.MORPH_ERODE, kernel)
+
+        return channel
         kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
         channel = cv.morphologyEx(channel, cv.MORPH_DILATE, kernel)
         channel = self._fill_holes(channel)
